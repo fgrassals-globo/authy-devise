7cd61769d2c9b71bb4630f06e9213adb38bff039
jdejong <jdejong@sparknettech.com>
2015-09-14 10:23:37 -0400

    Update authy_authenticatable.rb

--- lib/devise-authy/hooks/authy_authenticatable.rb
+++ lib/devise-authy/hooks/authy_authenticatable.rb
@@ -1,5 +1,5 @@
 Warden::Manager.after_authentication do |user, auth, options|
-  if user.respond_to?(:with_required_authy_authentication)
+  if user.respond_to?(:with_required_authy_authentication?)
     if auth.session(options[:scope])[:with_required_authy_authentication] = user.with_required_authy_authentication?(auth.request)
     end
   else
